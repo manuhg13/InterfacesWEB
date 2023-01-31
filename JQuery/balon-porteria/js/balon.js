53b93fbf22ac2e5cf76ff26a693a7c17dd25e232
Manuel <manuhg13@gmail.com>
2023-01-31 12:42:28 +0000

    d

--- JQuery/balon-porteria/js/balon.js
+++ JQuery/balon-porteria/js/balon.js
@@ -1,12 +1,14 @@
 $(document).ready(function () {
-    $('#balon').click(function (e) { 
+    $("#balon").click(function (e) { 
         e.preventDefault();
         
         $(this).animate({
             position: 'absolute',
-            bottom: '+=50px',
-            fill: 'red'
-        }, "slow");
-        $(this).animate({fill:'green'});
+            top: '-480px',
+        }, "slow" , function () { 
+            $('#balon').css({'fill':'green'});
+         });
+        $(this).css({'fill':'red'});
+        
     });
 });
