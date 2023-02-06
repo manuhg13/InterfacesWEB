3042dab6ce46d01a7877309bf3770cb48057129d
manu <manuhg13@gmail.com>
2023-02-06 22:42:00 +0100

    7

--- JQuery/calculadora/js/funciones.js
+++ JQuery/calculadora/js/funciones.js
@@ -2,38 +2,33 @@ $(document).ready(function () {
     let valor="0";
     let operacion="";
 
-    $('.col-4 btn btn-success').click(function (e) { 
+    $('button').click(function (e) { 
         e.preventDefault();
-        if(valor==="0"){
-            valor=$(this).text();
-        }else{
-            valor+=$(this).text();
+        if ($(this).text().match(/[0-9]+/)){
+            if(valor==="0"){
+                valor=$(this).text();
+            }else{
+                valor+=$(this).text();
+            }
+    
+            $('#pantalla').val(valor);
+        }else if($(this).text().match(/[\+\-\*\/]/)){
+            operacion+= valor + $(this).text();
+            valor="0";
+
+            $('#pantalla').val(operacion);
+        }else if($(this).text() === "C"){
+            valor="0";
+            operacion="";
+
+            $('#pantalla').val(valor);
+        }else if($(this).text() === "="){
+            operacion+= valor;
+            valor= eval(operacion);
+
+            $('#pantalla').val(valor);
         }
-
-        $('#pantalla').val(valor);
-    });
-
-    $('.col-4 btn btn-secondary').click(function (e) { 
-        e.preventDefault();
-        operacion+= valor + $(this).text();
-        valor="0";
-
-        $('#pantalla').val(operacion);
-    });
-
-    $('#borrar').click(function (e) { 
-        e.preventDefault();
-        valor="0";
-        operacion="";
-
-        $('#pantalla').val(valor);
-    });
-
-    $('#igual').click(function (e) { 
-        e.preventDefault();
-        operacion+= valor;
-        valor= eval(operacion);
-
-        $('#pantalla').val(valor);
     });
 });
+
+
